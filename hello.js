65117a7fb5e1326f35628f530a639beca6dbda5d
Akif Islam <iamakifislam@gmail.com>
2022-01-03 21:58:10 +0600

    Add Bootstrap

--- hello.js
+++ hello.js
@@ -1,15 +1,28 @@
 let plus = document.getElementById("increment")
 let neg = document.getElementById("decrement")
+let reset=document.getElementById("reset")
 let number = document.getElementById("display")
+let getRandom = document.getElementById("getrandom")
 
 let count = 0;
 
 plus.addEventListener('click',function (){
     count+=1
-    number.innerHTML = count;
+    number.innerHTML = count
 })
 
 neg.addEventListener('click',function (){
     count-=1
-    number.innerHTML = count;
+    number.innerHTML = count
 })
+
+reset.addEventListener('click',function(){
+    count = 0
+    number.innerHTML= count
+})
+
+getRandom.addEventListener('click',function(){
+    let x = Math.floor(Math.random()*100)
+    number.innerHTML = x
+})
+
